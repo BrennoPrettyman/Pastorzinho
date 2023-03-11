282a2f824a6ec8bcc7a7ccb74db66d2242397032
Brenno Prettyman <brennoprettyman@gmail.com>
2023-03-11 20:22:54 -0300

    Exercicio Resolvido

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Pastorzinho;
+class Program
+{
+    static void Main(string[] args)
+    {
+        #pragma warning disable CA1416
+
+//Dó
+        Console.Beep(1320, 200);
+        Thread.Sleep(100);
+//Ré
+        Console.Beep(1485, 200);
+        Thread.Sleep(100);
+//Mi
+        Console.Beep(1650, 200);
+        Thread.Sleep(100);
+//Fá...
+        Console.Beep(1759, 200);
+        Thread.Sleep(500);
+//Fá
+        Console.Beep(1759, 200);
+        Thread.Sleep(100);
+//Fá
+        Console.Beep(1759, 200);
+        Thread.Sleep(500);
+//Dó
+        Console.Beep(1320, 200);
+        Thread.Sleep(100);
+//Ré
+        Console.Beep(1485, 200);
+        Thread.Sleep(100);
+//Dó
+        Console.Beep(1320, 200);
+        Thread.Sleep(100);
+//Ré...
+        Console.Beep(1485, 200);
+        Thread.Sleep(500);
+//Ré
+        Console.Beep(1485, 200);
+        Thread.Sleep(100);
+//Ré...
+        Console.Beep(1485, 200);
+        Thread.Sleep(500);
+//Dó
+        Console.Beep(1320, 200);
+        Thread.Sleep(100);
+//Sol
+        Console.Beep(1980, 200);
+        Thread.Sleep(100);
+//Fá
+        Console.Beep(1759, 200);
+        Thread.Sleep(100);
+//Mi...
+        Console.Beep(1650, 200);
+        Thread.Sleep(500);
+//Mi
+        Console.Beep(1650, 200);
+        Thread.Sleep(100);
+//Mi...
+        Console.Beep(1650, 200);
+        Thread.Sleep(500);
+//Dó
+        Console.Beep(1320, 200);
+        Thread.Sleep(100);
+//Ré
+        Console.Beep(1485, 200);
+        Thread.Sleep(100);
+//Mi
+        Console.Beep(1650, 200);
+        Thread.Sleep(100);
+//Fá...
+        Console.Beep(1759, 200);
+        Thread.Sleep(500);
+//Fá
+        Console.Beep(1759, 200);
+        Thread.Sleep(100);
+//Fá
+        Console.Beep(1759, 200);
+        Thread.Sleep(100);
+    }
+}
